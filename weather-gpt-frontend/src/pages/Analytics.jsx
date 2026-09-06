43540fae1ec05a594b0ef64b7a1ff97d5a48a7a3
Cyrus rana <ranacyrus13@gmail.com>
2026-09-06 18:10:07 +0530

    feat:analytics

--- weather-gpt-frontend/src/pages/Analytics.jsx
+++ weather-gpt-frontend/src/pages/Analytics.jsx
@@ -24,7 +24,7 @@ import {
 } from 'lucide-react'
 
 
-const API_BASE_URL = 'http://127.0.0.1:8000'
+const API_BASE_URL = 'https://weather-gpt-zrn2.onrender.com'
 
 
 export default function Analytics({ location }) {
